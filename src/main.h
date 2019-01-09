af4a82c953c9f86274716060cac19c798e18dc9c
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-08 16:27:37 -0800

    Add networktables because the lib is messed up

--- src/main.h
+++ src/main.h
@@ -102,20 +102,20 @@ public:
 class NTListener : public ITableListener {
 private:
     std::map<std::string, double*> ntDoubleKeys {
-            {"max_vel_x", &teb_cfg.robot.max_vel_x},
-            {"max_vel_x_backwards", &teb_cfg.robot.max_vel_x_backwards},
-            {"acc_lim_x", &teb_cfg.robot.acc_lim_x},
-            {"max_vel_theta", &teb_cfg.robot.max_vel_theta},
-            {"acc_lim_theta", &teb_cfg.robot.acc_lim_theta},
+            {"--------max_vel_x",                       &teb_cfg.robot.max_vel_x},
+            {"--------max_vel_x_backwards",             &teb_cfg.robot.max_vel_x_backwards},
+            {"--------acc_lim_x",                       &teb_cfg.robot.acc_lim_x},
+            {"--------max_vel_theta",                   &teb_cfg.robot.max_vel_theta},
+            {"--------acc_lim_theta",                   &teb_cfg.robot.acc_lim_theta},
 //            {"weight_optimaltime", &teb_cfg.optim.weight_optimaltime},
-            {"goal_position_x", &goal_pose.position.x},
-            {"goal_position_y", &goal_pose.position.y},
-            {"goal_orientation_z", &goal_pose.orientation.z},
-            {"min_turning_radius", &teb_cfg.robot.min_turning_radius},
-            {"weight_kinematics_forward_drive", &teb_cfg.optim.weight_kinematics_forward_drive}
+            {"--------goal_position_x",                 &goal_pose.position.x},
+            {"--------goal_position_y",                 &goal_pose.position.y},
+            {"--------goal_orientation_z",              &goal_pose.orientation.z},
+            {"--------min_turning_radius",              &teb_cfg.robot.min_turning_radius},
+            {"--------weight_kinematics_forward_drive", &teb_cfg.optim.weight_kinematics_forward_drive}
     };
     std::map<std::string, bool*> ntBoolKeys {
-            {"free_goal_vel", &teb_cfg.goal_tolerance.free_goal_vel}
+            {"--------free_goal_vel", &teb_cfg.goal_tolerance.free_goal_vel}
     };
 public:
     NTListener(shared_ptr<NetworkTable> source);
